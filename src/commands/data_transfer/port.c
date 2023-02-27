314b6766e155724f098af2462c97de9a1c6951cb
CARTTIGUEANE Kévin <kevin.carttigueane@epitech.eu>
2023-02-27 14:20:54 +0100

    feat(myFTP): PORT | STOR

---
+++ src/commands/data_transfer/port.c
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2022
+** Repository-MyFTP
+** File description:
+** port.c
+*/
+
+#define PORT_ARGS_FORMAT "%[^,],%d,%d"
+
+#include "../../../include/server.h"
+
+int port_data_socket(char* ip, int port1, int port2)
+{
+    struct sockaddr_in client_address;
+    memset(&client_address, 0, sizeof(client_address));
+    client_address.sin_family = AF_INET;
+    client_address.sin_port = htons(port1 * 256 + port2);
+    inet_aton(ip, &client_address.sin_addr);
+
+    int data_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (data_socket_fd < 0)
+        handle_error("socket");
+
+    if (connect(data_socket_fd, (struct sockaddr*)&client_address,
+                sizeof(client_address)) < 0)
+        handle_error("connect");
+
+    return data_socket_fd;
+}
+
+void handle_port_args(int control_socket, client_t* clients,
+server_data_t* server_data, char** port_args)
+{
+    if (get_size_word_array(port_args) != 2) {
+        send_resp(control_socket, FTP_REPLY_501);
+        return;
+    }
+    char ip[BUFFER_SIZE];
+    int port1, port2;
+    int format_input =
+        sscanf(port_args[1], PORT_ARGS_FORMAT, ip, &port1, &port2);
+    if (format_input != 3) {
+        send_resp(control_socket, FTP_REPLY_501);
+        return;
+    }
+    server_data->data_socket_fd = port_data_socket(ip, port1, port2);
+    send_resp(clients[control_socket - 4].client_socked_fd, FTP_REPLY_200);
+}
+
+void port(int control_socket, ...)
+{
+    va_list args;
+    va_start(args, control_socket);
+    client_t* clients = get_nth_argument(1, args);
+    server_data_t* server_data = get_nth_argument(0, args);
+    char** port_args = get_nth_argument(2, args);
+
+    if (!is_logged(control_socket, clients, &args))
+        return;
+
+    handle_port_args(control_socket, clients, server_data, port_args);
+
+    va_end(args);
+}
---
+++ src/commands/data_transfer/stor.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2022
+** Repository-MyFTP
+** File description:
+** store.c
+*/
+
+#include "../../../include/server.h"
+
+void stor_file(int data_socket_temp, FILE* file)
+{
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    while ((bytes_received = read(data_socket_temp, buffer, BUFFER_SIZE)) > 0)
+        if (write(fileno(file), buffer, bytes_received) < 0)
+            handle_error("write");
+}
+
+void stor_cleanup(server_data_t* server_data, FILE* file, int data_socket_temp,
+va_list args)
+{
+    fclose(file);
+    close(data_socket_temp);
+    close(server_data->data_socket_fd);
+    server_data->data_socket_fd = FAILURE;
+    va_end(args);
+}
+
+void stor(int control_socket, ...)
+{
+    va_list args;
+    va_start(args, control_socket);
+    client_t* clients = get_nth_argument(1, args);
+    server_data_t* server_data = get_nth_argument(0, args);
+    char** commands = get_nth_argument(2, args);
+    if (!commands[1]) {
+        send_resp(control_socket, FTP_REPLY_501);
+        return;
+    }
+    if (!is_logged(control_socket, clients, &args)) return;
+    if (!is_data_con_establish(control_socket, server_data)) return;
+    int data_socket_temp = create_temp_socket(server_data);
+    FILE* file = fopen(commands[1], "w");
+    if (!file) return;
+    dprintf(control_socket, "150 Ok to send data.\r\n");
+    stor_file(data_socket_temp, file);
+    dprintf(control_socket, FTP_REPLY_426);
+    stor_cleanup(server_data, file, data_socket_temp, args);
+}
